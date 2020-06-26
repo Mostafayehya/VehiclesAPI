d62496fc60cf8014addac5f1c26ba45b332e324b
MostafaYahia <mostafayehyax23@gmail.com>
2020-06-26 13:21:20 +0200

    Adding Integration tests to Price-Service of VehicleAPI project

--- pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
+++ pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
@@ -3,10 +3,10 @@ package com.udacity.pricing.domain.price;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
+
 @RepositoryRestResource(path ="price")
 interface PriceRepository extends CrudRepository<Price, Long> {
-
-
 
 
 }
